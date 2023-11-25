e88ac5065511844e3026312c4a6c21badbc9a26d
Rahim <rahim.nagori@gmail.com>
2023-11-26 01:00:51 +0530

    file formatting updated

--- application/views/site/include/header.php
+++ application/views/site/include/header.php
@@ -2,75 +2,79 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title> Abdul Rahim Nagori </title>
-  <!-- favicon -->
-  <!-- <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon"> -->
-  <!-- bootstrap -->
-  <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_bootstrap.min.css">
-  <!-- Plugin css -->
-  <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_plugin.css">
-  <!-- Flaticon -->
-  <link rel="stylesheet" type="text/css" href="<?=site_url('assets/site/');?>css/rahim_flaticon.css">
-  <!-- stylesheet -->
-  <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_style.css?time=<?=time(); ?>">
-  <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_multicolor.css">
-  <!-- responsive -->
-  <link rel="stylesheet" type="text/css" href="<?=site_url('assets/site/');?>css/rahim_font-awesome.min.css">
-  <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_responsive.css">
-  <link rel="shortcut icon" type="image/jpg" href="<?=site_url('assets/site/');?>img/logo_foopter.png"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Abdul Rahim Nagori </title>
+    <!-- favicon -->
+    <!-- <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon"> -->
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_bootstrap.min.css">
+    <!-- Plugin css -->
+    <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_plugin.css">
+    <!-- Flaticon -->
+    <link rel="stylesheet" type="text/css" href="<?=site_url('assets/site/');?>css/rahim_flaticon.css">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_style.css?time=<?=time(); ?>">
+    <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_multicolor.css">
+    <!-- responsive -->
+    <link rel="stylesheet" type="text/css" href="<?=site_url('assets/site/');?>css/rahim_font-awesome.min.css">
+    <link rel="stylesheet" href="<?=site_url('assets/site/');?>css/rahim_responsive.css">
+    <link rel="shortcut icon" type="image/jpg" href="<?=site_url('assets/site/');?>img/logo_foopter.png" />
 </head>
 
 <body>
-  <!-- preloader area start -->
-  <!-- <div class="preloader" id="preloader">
+    <!-- preloader area start -->
+    <!-- <div class="preloader" id="preloader">
     <div class="loader loader-1">
       <div class="loader-outter"></div>
       <div class="loader-inner"></div>
     </div>
   </div> -->
-  <!-- preloader area end -->
+    <!-- preloader area end -->
 
 
-  <!--Main-Menu Area Start-->
-  <div class="side-menu-wrapper">
-    <div class="menu-toogle-icon">
-      <div id="nav-icon3">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-    <div class="side-menu">
-      <a href="#" class="profile-photo" style="padding: 10px 10px; display: inline-block;"  >
-          <img src="<?=site_url('assets/site/');?>img/logo.png" alt="" class="wow zoomIn" data-wow-delay="0.2s" style="object-fit: contain;background: #fff;">
-        </a>
-      <ul id="mainmenu-area">
-        <li class="current">
-          <a href="#home" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-home"></i>Home</a>
-        </li>
-        <li>
-          <a href="#about" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-user"></i>About</a>
-        </li>
-        <li>
-          <a href="#service" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-briefcase"></i>Services</a>
-        </li>
-       <!--  <li>
+    <!--Main-Menu Area Start-->
+    <div class="side-menu-wrapper">
+        <div class="menu-toogle-icon">
+            <div id="nav-icon3">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <div class="side-menu">
+            <a href="#" class="profile-photo" style="padding: 10px 10px; display: inline-block;">
+                <img src="<?=site_url('assets/site/');?>img/logo.png" alt="" class="wow zoomIn" data-wow-delay="0.2s"
+                    style="object-fit: contain;background: #fff;">
+            </a>
+            <ul id="mainmenu-area">
+                <li class="current">
+                    <a href="#home" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-home"></i>Home</a>
+                </li>
+                <li>
+                    <a href="#about" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-user"></i>About</a>
+                </li>
+                <li>
+                    <a href="#service" class="wow fadeInUp" data-wow-delay="0.4s"><i
+                            class="fa fa-briefcase"></i>Services</a>
+                </li>
+                <!--  <li>
           <a href="#resume" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-file-text"></i>Resume</a>
         </li> -->
-        <li>
-          <a href="#project-gallery" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-file-image-o"></i>Portfolio</a>
-        </li>
-        <li>
-          <a href="#blog" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-rss"></i>Blog</a>
-        </li>
-        <li>
-          <a href="#contact" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-whatsapp"></i>Contact</a>
-        </li>
-      </ul>
+                <li>
+                    <a href="#project-gallery" class="wow fadeInUp" data-wow-delay="0.4s"><i
+                            class="fa fa-file-image-o"></i>Portfolio</a>
+                </li>
+                <li>
+                    <a href="#blog" class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-rss"></i>Blog</a>
+                </li>
+                <li>
+                    <a href="#contact" class="wow fadeInUp" data-wow-delay="0.4s"><i
+                            class="fa fa-whatsapp"></i>Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
-  <!--Main-Menu Area Start-->
+    <!--Main-Menu Area Start-->
